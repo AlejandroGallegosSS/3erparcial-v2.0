9cf41d17321017836ba8ff01ef8c78d46cb7c76e
AlejandroGallegosSS <168442472+AlejandroGallegosSS@users.noreply.github.com>
2024-06-10 10:56:11 -0600

    inciso d)

---
+++ programa 54 c++ inciso d).cpp
@@ -0,0 +1,13 @@
+//insiso d)
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	int c, pvz;
+	int dec1,dec2;
+	double entero;
+	c=pow(entero,dec1);
+	cout<<" la potencia del numero natural y el numero decimal1 es :"<<c;
+	pvz=pow(entero,dec2);
+	cout<<" la potencia del numero natural y el decimal2 es :"<<pvz;
+}
